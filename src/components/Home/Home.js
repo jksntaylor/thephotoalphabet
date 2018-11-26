9fc68a756e0c7d8ae381d963213cb232920a61a7
unknown <josephjacksontaylor@gmail.com>
2018-11-26 14:47:52 -0700

    added handwriting component to home.js, animated svg

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import Handwriting from '../Handwriting/Handwriting'
+import '../../handwriting.css'
 
 class Home extends Component {
     render() {
+
         return (
             <div>
-                <div>The Photo Alphabet</div>
+                <Handwriting />
                 <Link to='/make'><button>Get Started</button></Link>
             </div>
         )
