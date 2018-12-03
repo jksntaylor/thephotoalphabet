5672c2c8c89165c40f0e14c6095cff92ac6cbcb1
unknown <josephjacksontaylor@gmail.com>
2018-12-03 14:22:14 -0700

    added magnify toggle and animation

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -9,6 +9,7 @@ class Input extends Component {
         super();
         this.state = {
             userInput: [],
+            magnified: false
         }
     }
 
@@ -62,6 +63,12 @@ class Input extends Component {
         
     }
 
+    toggleMagnified = () => {
+        this.setState({
+            magnified: !this.state.magnified
+        })
+    }
+
     render() {
         if (this.state.userInput) {
             var photos = this.state.userInput.map((obj, index) => {
@@ -74,26 +81,49 @@ class Input extends Component {
             inputValue = '';
         }
 
+         
+        let normal= <div className='inputPageContainer'>
+                        <div className='inputNavContainer'>
+                            <Link to='/auth'><i className="fas fa-user fa-2x"></i></Link>
+                            <Link to='/cart'><i className="fas fa-shopping-cart fa-2x"></i></Link>
+                        </div>
+                        <div className='randomContainer'>
+                            <div className='photosContainer'>
+                                {photos}
+                            </div>
+                            <i className="fas fa-random fa-2x" onClick={this.handleRandomize}></i>
+                            <i class="fas fa-search fa-2x" onClick={this.toggleMagnified}></i>
+                        </div>
+                        <div className='inputContainer'>
+                            <input className='input' placeholder='Type Here!' value={inputValue} onChange={e => this.handleInputChange(e.target.value)} type="text" />
+                            {this.state.userInput.length>=3 ? <div><button onClick={this.addToCart} className='addToCartButton'>Add To Cart</button></div> : <div className='inputMinLength'><h6 >Word Must be At Least 3 Letters</h6></div>}
+                        </div>
+                    </div>;
+
+        let magnified = <div className='inputPageContainer'>
+                            <div className='inputNavContainer navMagnified'>
+                                <Link to='/auth'><i className="fas fa-user fa-2x"></i></Link>
+                                <Link to='/cart'><i className="fas fa-shopping-cart fa-2x"></i></Link>
+                            </div>
+                            <div className='randomContainer randomMagnified'>
+                                <div className='photosContainer containerMagnified'>
+                                    {photos}
+                                </div>
+                                <i className="fas fa-random fa-2x faMagnified" onClick={this.handleRandomize}></i>
+                                <i class="fas fa-search fa-2x faMagnifiedToggle" onClick={this.toggleMagnified}></i>
+                            </div>
+                            <div className='inputContainer inputMagnified'>
+                                <input className='input' placeholder='Type Here!' value={inputValue} onChange={e => this.handleInputChange(e.target.value)} type="text" />
+                                {this.state.userInput.length>=3 ? <div><button onClick={this.addToCart} className='addToCartButton'>Add To Cart</button></div> : <div className='inputMinLength'><h6 >Word Must be At Least 3 Letters</h6></div>}
+                            </div>
+                        </div>;
 
 
 
         return (
-            <div className='inputPageContainer'>
-                <div className='inputNavContainer'>
-                    <Link to='/auth'><i className="fas fa-user fa-2x"></i></Link>
-                    <Link to='/cart'><i className="fas fa-shopping-cart fa-2x"></i></Link>
-                </div>
-                <div className='randomContainer'>
-                    <div className='photosContainer'>
-                        {photos}
-                    </div>
-                    <i className="fas fa-random fa-2x" onClick={this.handleRandomize}></i>
-                </div>
-                <div className='inputContainer'>
-                    <input className='input' placeholder='Type Here!' value={inputValue} onChange={e => this.handleInputChange(e.target.value)} type="text" />
-                    {this.state.userInput.length>=3 ? <div><button onClick={this.addToCart} className='addToCartButton'>Add To Cart</button></div> : <div className='inputMinLength'><h6 >Word Must be At Least 3 Letters</h6></div>}
-                </div>
-            </div>
+            <div>
+                {this.state.magnified ? magnified : normal}
+            </div> 
         )
     }
 }
