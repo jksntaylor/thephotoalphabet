a73ef119ebf2ffa0f57fa6d753111d092c5187a5
jksntaylor <josephjacksontaylor@gmail.com>
2019-12-27 14:37:35 -0700

    beginning restructure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import './App.css';
 import React, { Component } from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Home from './components/Home/Home';
-import Input from './components/Input/Input';
-// import Menu from './components/Menu/Menu';
-// import Checkout from './components/Checkout/Checkout';
 import Cart from './components/Cart/Cart';
 import Auth from './components/Auth/Auth';
 
@@ -23,16 +19,11 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        {/* <Menu/> */}
         <Switch>
           <Route exact path='/' render={(props) => <Home {...props} user={this.state.user} updateUser={this.updateUser} /> } />
-          <Route path='/make' render={(props) => <Input {...props} user={this.state.user} updateUser={this.updateUser} /> } />
           <Route path='/cart' render={(props) => <Cart {...props} user={this.state.user} updateUser={this.updateUser} /> } />
-          {/* <Route path='/checkout' render={(props) => <Checkout {...props} user={this.state.user} updateUser={this.updateUser} /> } /> */}
           <Route path='/auth' render={(props) => <Auth {...props} user={this.state.user} updateUser={this.updateUser} /> } />
         </Switch>
-      </div>
     );
   }
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,23 +1,61 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
-import Handwriting from '../Handwriting/Handwriting'
-import '../../handwriting.css';
-import './home.css'
+import React from 'react';
+import InputPhoto from '../InputPhoto/InputPhoto';
+import './home.css';
+import axios from 'axios';
 
-class Home extends Component {
+export default class Home extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            userInput: [],
+            inputWord: ''
+        }
+    }
+
+    handleDelete = () => {
+        let {userInput, inputWord} = this.state;
+        let newInput = inputWord.slice(0, inputWord.length-1)
+        this.setState({userInput: userInput.pop(), inputWord: newInput})
+    }
+
+    handleInputChange = val => {
+        if (!val) {this.setState({userInput: [], inputWord: ''}); return;}
+        if (val.length<this.state.inputWord.length) this.handleDelete();
+        if (!val.match(/[a-z]/i)) return;
+        if (val.length>10) { this.setState({error: "10 Letter Limit"}); return; }
+        let userInput = val.split('').reduce((acc, letter) => {
+            return [...acc, {letter: letter.toUpperCase(), count: 1}]
+        }, [])
+        this.setState({userInput: userInput, inputWord: val})
+    }
+
+    addToCart = () => { axios.post(`/cart/${this.state.userInput}`).then(() => {this.setState({userInput: ''})}) }
+
     render() {
+        const {userInput, inputWord} = this.state;
+        let price = (30 + (inputWord.length-3)*5) - 0.01;
+        let photos = userInput.length>=3 ? 
+        userInput.map((obj, index) => {
+            return (
+                <InputPhoto count={obj.count} letter={obj.letter} key={`${obj.letter}${obj.count}${index}`}/>
+            )
+        }) : null
         return (
-            <div className='home-container'>
-                <div className='home-handwriting-container'>
-                    <Handwriting />
+            <div className='inputPageContainer'>    
+                <div className={'photosContainer'}>
+                    {photos}
                 </div>
-                <div className='home-button-container'>
-                    <Link to='/make' className='home-button-anchor'><button className='home-button'>GET STARTED</button></Link>
-                    <div className='home-button-underline'/>
+                <div className='inputContainer'>
+                    <input className='input' placeholder='Type Here!' value={inputWord} onChange={e => this.handleInputChange(e.target.value)} type="text" />
+                    {userInput.length>=3 ? 
+                    <div className='inputPriceContainer'>
+                        <h3 className='inputPrice'>Price: ${price}</h3>
+                        <button onClick={this.addToCart} className='addToCartButton'>Add To Cart</button>
+                    </div> 
+                    : 
+                    <div className='inputMinLength'><h6 >Word Must be At Least 3 Letters</h6></div>}
                 </div>
             </div>
         )
     }
 }
-
-export default Home
