ca0124b7aa8ed7960b18e1864688be79f9277f9a
jksntaylor <josephjacksontaylor@gmail.com>
2019-12-27 14:47:16 -0700

    cleaning up app and index in src

--- src/App.js
+++ src/App.js
@@ -1,29 +1,19 @@
 import React, { Component } from 'react';
-import {Switch, Route} from 'react-router-dom';
 import Home from './components/Home/Home';
-import Cart from './components/Cart/Cart';
-import Auth from './components/Auth/Auth';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      user: {}
-    }
-    this.updateUser = this.updateUser.bind(this);
+    this.state = {user: {}}
   }
 
-  updateUser (user) {
+  updateUser = user => {
     this.setState({user})
   }
 
   render() {
     return (
-        <Switch>
-          <Route exact path='/' render={(props) => <Home {...props} user={this.state.user} updateUser={this.updateUser} /> } />
-          <Route path='/cart' render={(props) => <Cart {...props} user={this.state.user} updateUser={this.updateUser} /> } />
-          <Route path='/auth' render={(props) => <Auth {...props} user={this.state.user} updateUser={this.updateUser} /> } />
-        </Switch>
+        <Home user={this.state.user} updateUser={this.updateUser} />
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {Provider} from 'react-redux';
-import {BrowserRouter} from 'react-router-dom';
 import store from './redux/store';
 
 ReactDOM.render(
     <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <App />
     </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
